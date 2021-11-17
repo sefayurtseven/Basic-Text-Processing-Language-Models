6d6cd976c1e66a2f0409277bc96b65d991d85f03
Sefa <sefa.yurtseven@simbt.com>
2021-11-18 00:49:22 +0300

    smooting try

--- ReadFile.py
+++ ReadFile.py
@@ -10,12 +10,18 @@ class FileReader(object):
     file_name = ""
     sentences_list = []
     sentences_list_padding = []
+    sentences_list_padding_via_unk = []
     word_list = []
+    word_list_via_unk = []
     freq_dict = {}
+    freq_dict_via_unk = {}
     bigrams_lst = []
+    bigrams_lst_via_unk = []
     bigram_dict = {}
+    bigram_dict_via_unk = {}
     unigram_prob_dict = {}
     bigram_prob_dict = {}
+    bigram_prob_dict_via_unk = {}
     num_of_token = 0
 
     def __init__(self, filepath, filename):
@@ -30,6 +36,11 @@ class FileReader(object):
         self.create_bigram_dict()
         self.create_bigram_prob_dict()
         self.create_unigram_prob_dict()
+        self.change_less_freq_words_vith_unk()
+        self.word_tokenize_via_unk()
+        self.get_word_frequency_via_unk()
+        self.get_bigrams_list_unk()
+        self.create_bigram_dict_via_unk()
 
 
     def read_txt_file(self):
@@ -43,6 +54,10 @@ class FileReader(object):
     def word_tokenize(self):
         for sentences in self.sentences_list_padding:
             self.word_list.append(sentences)
+
+    def word_tokenize_via_unk(self):
+        for sentences in self.sentences_list_padding_via_unk:
+            self.word_list_via_unk.append(sentences)
 
     def print_sentences(self):
         print("--------- * Sentences * ---------")
@@ -72,9 +87,32 @@ class FileReader(object):
                     self.freq_dict[w] = 1
         self.freq_dict = {k: v for k, v in sorted(self.freq_dict.items(), key=lambda item: item[1])}
 
+    def get_word_frequency_via_unk(self):
+        for w_list in self.word_list_via_unk:
+            for w in w_list:
+                if self.freq_dict_via_unk.__contains__(w):
+                    self.freq_dict_via_unk[w] = self.freq_dict_via_unk[w] + 1
+                else:
+                    self.freq_dict_via_unk[w] = 1
+        self.freq_dict_via_unk = {k: v for k, v in sorted(self.freq_dict_via_unk.items(), key=lambda item: item[1])}
+
+    def get_word_frequency(self):
+        for w_list in self.word_list:
+            self.num_of_token += len(w_list)
+            for w in w_list:
+                if self.freq_dict.__contains__(w):
+                    self.freq_dict[w] = self.freq_dict[w] + 1
+                else:
+                    self.freq_dict[w] = 1
+        self.freq_dict = {k: v for k, v in sorted(self.freq_dict.items(), key=lambda item: item[1])}
+
     def get_bigrams_list(self):
         for padding_sent in self.sentences_list_padding:
             self.bigrams_lst.append(list(bigrams(padding_sent)))
+
+    def get_bigrams_list_unk(self):
+        for padding_sent in self.sentences_list_padding_via_unk:
+            self.bigrams_lst_via_unk.append(list(bigrams(padding_sent)))
 
     def get_sentences_num(self):
         return len(self.sentences_list)
@@ -91,7 +129,21 @@ class FileReader(object):
                     self.bigram_dict[b] = 1
         self.bigram_dict = {k: v for k, v in sorted(self.bigram_dict.items(), key=lambda item: item[1])}
 
+    def create_bigram_dict_via_unk(self):
+        for b_list in self.bigrams_lst_via_unk:
+            for b in b_list:
+                if self.bigram_dict_via_unk.__contains__(b):
+                    self.bigram_dict_via_unk[b] = self.bigram_dict_via_unk[b] + 1
+                else:
+                    self.bigram_dict_via_unk[b] = 1
+        self.bigram_dict_via_unk = {k: v for k, v in sorted(self.bigram_dict_via_unk.items(), key=lambda item: item[1])}
+
     def create_bigram_prob_dict(self):
+        for b in self.bigram_dict.keys():
+            prob = self.bigram_dict[b] / self.freq_dict[b[0]]
+            self.bigram_prob_dict[b] = prob
+
+    def create_bigram_via_unk_prob_with_addk_dict(self):
         for b in self.bigram_dict.keys():
             prob = self.bigram_dict[b] / self.freq_dict[b[0]]
             self.bigram_prob_dict[b] = prob
@@ -101,6 +153,13 @@ class FileReader(object):
             prob = self.freq_dict[w] / self.num_of_token
             self.unigram_prob_dict[w] = prob
 
+    def change_less_freq_words_vith_unk(self):
+        self.sentences_list_padding_via_unk = self.sentences_list_padding.copy()
+        for less_freq_word in list(self.freq_dict.keys())[:3]:
+            for sent in self.sentences_list_padding_via_unk:
+                if sent.__contains__(less_freq_word):
+                    index = sent.index(less_freq_word)
+                    sent[index] = "unk"
 
     def show_results(self):
         self.print_sentences_via_padding()
@@ -113,7 +172,7 @@ class FileReader(object):
         for b in list(self.unigram_prob_dict.keys().__reversed__())[:10]:
             print("(\"" + str(b) + "\")      " + str(self.freq_dict[b]) + "        " + str(self.unigram_prob_dict[b]), end=" ")
             print("\n")
-        print("\nTop 10 Unigrams with Highest Frequencies:")
+        print("\nTop 10 Bigrams with Highest Frequencies:")
         for b in list(self.bigram_dict.keys().__reversed__())[:10]:
             print("(" + str(b[0]) + "," + str(b[1]) + ")      " + str(self.bigram_dict[b]) + "        " + str(self.bigram_prob_dict[b]), end=" ")
             print("\n")
